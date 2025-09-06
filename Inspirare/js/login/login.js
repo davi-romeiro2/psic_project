278b7c4eb1324ac7f85b57cc92dc48cf7084bba8
Davi Romeiro <134007005+davi-romeiro2@users.noreply.github.com>
2025-09-05 22:29:33 -0300

    Delete Inspirare directory

--- Inspirare/js/login/login.js
+++
@@ -1,8 +0,0 @@
-const senhaInput = document.getElementById("senha");
-const toggleSenha = document.getElementById("toggleSenha");
-
-toggleSenha.addEventListener("click", () => {
-  const isPassword = senhaInput.type === "password";
-  senhaInput.type = isPassword ? "text" : "password";
-  toggleSenha.src = isPassword ? "../img/eye_opened.svg" : "../img/eye_closed.svg";
-});
